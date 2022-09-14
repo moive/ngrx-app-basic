3a7997db9697e6b54f18e35dc70b9eb124d57589
Moises Velasquez <mvelasquez@pictoric.com>
2022-09-14 10:24:05 -0500

    optimize createReducer

--- src/app/counter/counter.reducer.ts
+++ src/app/counter/counter.reducer.ts
@@ -1,13 +1,22 @@
-import { Action } from '@ngrx/store';
-import { decrement, increment } from './counter.action';
+import { createReducer, on, Action } from '@ngrx/store';
+import { decrement, increment, reset } from './counter.action';
 
-export function counterReducer(state: number = 10, action: Action) {
-  switch (action.type) {
-    case increment.type:
-      return state + 1;
-    case decrement.type:
-      return state - 1;
-    default:
-      return state;
-  }
-}
+// export function counterReducer(state: number = 10, action: Action) {
+//   switch (action.type) {
+//     case increment.type:
+//       return state + 1;
+//     case decrement.type:
+//       return state - 1;
+//     default:
+//       return state;
+//   }
+// }
+
+const initialState = 20;
+
+export const counterReducer = createReducer(
+  initialState,
+  on(increment, (state) => state + 1),
+  on(decrement, (state) => state - 1),
+  on(reset, (state) => 0)
+);
