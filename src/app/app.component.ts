2fe7aaaa2499f845d6d8131ff6381c086031a9ca
Moises Velasquez <mvelasquez@pictoric.com>
2022-09-13 23:41:01 -0500

    updated app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'ngrx-app-basic';
+
+  counter: number;
+
+  constructor() {
+    this.counter = 10;
+  }
+
+  decrement() {
+    this.counter -= 1;
+  }
+
+  increment() {
+    this.counter += 1;
+  }
 }
