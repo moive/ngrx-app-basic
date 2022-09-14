170c2de3a1d7445f1fa197c878aca32b22d38a35
Moises Velasquez <mvelasquez@pictoric.com>
2022-09-14 10:06:14 -0500

    added Store DevTools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,21 @@ import { GrandsonComponent } from './counter/grandson/grandson.component';
 
 // NgRx
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
 import { counterReducer } from './counter/counter.reducer';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [AppComponent, SonComponent, GrandsonComponent],
-  imports: [BrowserModule, StoreModule.forRoot({ counter: counterReducer })],
+  imports: [
+    BrowserModule,
+    StoreModule.forRoot({ counter: counterReducer }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production, // REstrict extention to log-only
+    }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
